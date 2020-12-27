daa25c2f1333fb251d1a0250b602ed08463883ef
vivekvankadaru <vivekvankadaru.fit@gmail.com>
2020-12-27 18:41:28 +0530

    modified 5.py

--- datatypes_list/5.py
+++ datatypes_list/5.py
@@ -1,1 +1,12 @@
+'''
+Write a Python program to count the number of strings where the string length is 2 or more and the first and last character 
+are same from a given list of strings
+Sample List : ['abc', 'xyz', 'aba', '1221']
+Expected Result : 2
+'''
 
+
+#l=['abc', 'xyz', 'aba', '1221']
+l=input().split()
+print(l)
+print(len([i for i in l if len(i)>2 and i[0]==i[-1]]))
